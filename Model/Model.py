3be3d8194cb6c07583bcd467e0f71bdf623a30c9
CristianChipana <cristianchipanahuaman@gmail.com>
2022-08-12 18:22:19 -0500

    saas

--- Model/Model.py
+++ Model/Model.py
@@ -101,7 +101,10 @@ class Model:
             print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
             print(actividad)
             print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
-            return actividad
+            if actividad:
+                return actividad
+            else:
+                return []
         except Exception as e:
             print(e)
             return False
